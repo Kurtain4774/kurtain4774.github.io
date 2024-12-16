5a8aa9859d2cff7dd3ed4765fac31a4743d3794d
Kurtis <kmquant@yahoo.com>
2024-12-16 13:36:46 -0800

    V0.07
    Added functionality for light mode and dark mode scrolling

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ let color2 = { value: [0, 0, 0], change: [0, 0, 0], target: [0, 0, 0] };
 let darkMode = 0;
 let lightMode = 0;
 
-//start time since the window was opened in milliseconds
-let startTime = null;
+let scrollDelay = 0;
+let time = 0;
+
+let themeSelected = 0;
 
 //boolean to keep track of the state of the side nav bar
 let menuOpen = false;
@@ -17,6 +19,15 @@ const overlay = document.getElementById('overlay');
 const btn = document.querySelector('#btn');
 const sideNav = document.querySelector('#side-bar');
 const body = document.body;
+const background = document.getElementById("background");
+const lightModePercent = document.getElementById('light-mode-percentage-text');
+const darkModePercent = document.getElementById('dark-mode-percentage-text');
+const lightModeContainer = document.getElementById('light-container');
+const darkModeContainer = document.getElementById('dark-container');
+const percentageElements = document.getElementsByClassName('percentage-container');
+const arrows = document.getElementsByClassName('arrow');
+const buttonContainer = document.getElementById("button-container");
+const button = document.getElementById("btn")
 
 //start the animation
 requestAnimationFrame(animate);
@@ -100,12 +111,32 @@ function generateNewTarget(current){
 
 //update values and set the colors to the new values
 function animate(timestamp){
-    if(!startTime) startTime = timestamp;
+    time++;
+    if(scrollDelay > 0){
+        scrollDelay--;
+        time = 0;
+        //console.log(scrollDelay);
+    } else {
+        updateValues();
+        updateColor();
+        if(time % 10 == 0){
+            lightMode = Math.round(Math.max(lightMode-time / 50,0));
+            darkMode = Math.round(Math.max(darkMode-time / 50,0));
+        }
+        
+        updatePercentageText();
+    }
 
-    updateValues();
-    updateColor();
+    
+    if(themeSelected == 0){
+        requestAnimationFrame(animate);
+    }
+    
+}
 
-    requestAnimationFrame(animate);
+function updatePercentageText(){
+    lightModePercent.textContent = (Math.round(lightMode * 10)/10).toString() + '%';
+    darkModePercent.textContent = (Math.round(darkMode * 10)/10).toString() + '%';
 }
 
 //Close side nav if the user clicks away from the side nav
@@ -122,19 +153,47 @@ function createColor(color){
 
 //function sets the css values to the newly updated js values
 function updateColor(){
-    let c1 = createColor(color1.value);
-    let c2 = createColor(color2.value);
-
-
-    document.getElementById("background").style.background = `radial-gradient(${c1},${c2})`;
-
-    document.getElementById("btn").style.border =  `2px solid ${c1}`;
-
-    document.getElementById("btn").style.border =  `2px solid ${c1}`;
-
-    document.getElementById("button-container").style.borderImage = `linear-gradient(to right, ${c1},${c2}) 1`
-
+    if(themeSelected == 0){
+        let c1 = createColor(color1.value);
+        let c2 = createColor(color2.value);
     
+    
+        background.style.background = `radial-gradient(${c1},${c2})`;
+    
+        button.style.border =  `2px solid ${c1}`;
+    
+        buttonContainer.style.borderImage = `linear-gradient(to right, ${c1},${c2}) 1`
+    
+        let luminance = 0.2126 * (color1.value[0]/255) + 0.7152 * (color1.value[1]/255) + 0.0722 * (color1.value[2]/255);
+    
+        //console.log(luminance);
+    
+        if(luminance >= 0.7){
+            for(let i = 0; i < percentageElements.length; i++){
+                percentageElements[i].style.color = 'black';
+            }
+            for(let i = 0; i < arrows.length; i++){
+                arrows[i].style.borderColor = 'black';
+            }
+        } else if(luminance <= 0.3){
+            for(let i = 0; i < percentageElements.length; i++){
+                percentageElements[i].style.color = 'white';
+            }
+            for(let i = 0; i < arrows.length; i++){
+                arrows[i].style.borderColor = 'white';
+            }
+        }
+    } else if(themeSelected == 1){
+        background.style.background = 'white';
+        color1.value = [255,255,255];
+        color2.value = [255,255,255];
+    } else if(themeSelected == 2){
+        background.style.background = 'black';
+        color1.value = [0,0,0];
+        color2.value = [0,0,0];
+    }
+    
+
 }
 
 //return int between 0 and max
@@ -147,8 +206,22 @@ function printColor(color){
     console.log(color[0] + " " + color[1] + " " + color[2]);
 }
 
+function reset(){
+    requestAnimationFrame(animate);
+    themeSelected = 0;
+    lightModeContainer.classList.toggle('hidden');
+    darkModeContainer.classList.toggle('hidden');
+    lightMode = 0;
+    darkMode = 0;
+    updatePercentageText();
+}
+
 //generates a new gradient
 function changeColor(){
+    if(themeSelected != 0){
+        reset();
+    }
+    
 
     let min = 255;
     do{
@@ -177,21 +250,70 @@ function updateValue(value, change){
 
 //gets the distance scrolled and update the colors based on the distance
 function scroll(event){
-    let change = -event.deltaY / 15.0;
+    if(themeSelected == 0){
+        let change = -event.deltaY / 10.0;
 
-    color1.value = updateValue(color1.value,[change,change,change]);
-    color2.value = updateValue(color2.value,[change,change,change]);
+        color1.value = updateValue(color1.value,[change,change,change]);
+        color2.value = updateValue(color2.value,[change,change,change]);
+    
+        updateColor();
+    
+        change /= 2.5;
+    
+        if(change > 0){
+            lightMode+=change;
+            darkMode-=change;
+        } else {
+            darkMode+= -change;
+            lightMode-= -change;
+        }
+    
+        lightMode = Math.max(Math.min(100,lightMode),0);
+        darkMode = Math.max(Math.min(100,darkMode),0);
+    
+        scrollDelay = Math.min(scrollDelay + 25, 400);
+    
+        updatePercentageText();
 
-    updateColor();
+        console.log(lightMode);
+    
+        if(lightMode == 100){
+            themeSelected = 1;
+            lightModeContainer.classList.toggle('hidden');
+            darkModeContainer.classList.toggle('hidden');
+            background.style.backgroundColor = 'white';
+            buttonContainer.style.border = '2px solid black';
+            btn.style.border = '2px solid black';
+            updateColor();
+        }
+        if(darkMode == 100){
+            themeSelected = 2;
+            lightModeContainer.classList.toggle('hidden');
+            darkModeContainer.classList.toggle('hidden');
+            background.style.backgroundColor = 'black';
+            updateColor();
+        }
+    }
+    
 }
+
 
 //changes color of center button on hover
 btn.addEventListener("mouseenter", function( event ) {   
     let c1 = createColor(color1.value);
     event.target.style.backgroundColor = `${c1}`;
+    if(color1.value[0] + color1.value[1] + color1.value[2] == 765){
+        event.target.style.backgroundColor = `${createColor([211,211,211])}`;
 
-    if(color1.value[0] + color1.value[1] + color1.value[2] > 600){
+    }
+
+    let luminance = 0.2126 * (color1.value[0]/255) + 0.7152 * (color1.value[1]/255) + 0.0722 * (color1.value[2]/255);
+
+    if(luminance >= 0.5){
+        event.target.style.color = "black";
+    } else {
         event.target.style.color = "white";
+
     }
   }, false);
 
